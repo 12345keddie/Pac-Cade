9716d4b20c659f4eec4bd3cac9cdfb77c248eb47
12345keddie <leokeddie1@gmail.com>
2025-11-23 13:11:21 +0000

    Unblock EmulatorJS on Vercel: remove COEP/COOP; use cdn.emulatorjs.com

--- app.js
+++ app.js
@@ -447,7 +447,7 @@ async function startEmulator(core, file, theme) {
   window.EJS_enable_sound = true;
   window.EJS_gamepad = true;
   const s = document.createElement('script');
-  s.src = 'https://cdn.emulatorjs.org/stable/data/loader.js';
+  s.src = 'https://cdn.emulatorjs.com/loader.js';
   try { s.crossOrigin = 'anonymous'; } catch {}
   s.async = true;
   s.onload = () => console.log('Emulator loaded');
@@ -473,7 +473,7 @@ async function startEmulatorUrl(core, url, theme) {
   window.EJS_enable_sound = true;
   window.EJS_gamepad = true;
   const s = document.createElement('script');
-  s.src = 'https://cdn.emulatorjs.org/stable/data/loader.js';
+  s.src = 'https://cdn.emulatorjs.com/loader.js';
   try { s.crossOrigin = 'anonymous'; } catch {}
   s.async = true;
   s.onload = () => console.log('Emulator loaded');
